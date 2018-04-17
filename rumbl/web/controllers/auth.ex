971dd9bcc4f6ba51d76c9c0573f1234bf1f6bf62
Choadis <dahlgrensm@gmail.com>
2018-04-17 03:05:25 -0400

    Finished Chapter 10

--- rumbl/web/controllers/auth.ex
+++ rumbl/web/controllers/auth.ex
@@ -1,6 +1,8 @@
 defmodule Rumbl.Auth do
   import Plug.Conn
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+  import Phoenix.Controller
+  alias Rumbl.Router.Helpers
 
   def init(opts) do
     Keyword.fetch!(opts, :repo)
@@ -8,18 +10,30 @@ defmodule Rumbl.Auth do
 
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
+
     cond do
-      user = conn.assigns[:current_user] -> conn
-      user = user_id && repo.get(Rumbl.User, user_id) -> assign(conn, :current_user, user)
-      true -> assign(conn, :current_user, nil)
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
+      user = user_id && repo.get(Rumbl.User, user_id) ->
+        put_current_user(conn, user)
+      true ->
+        assign(conn, :current_user, nil)
     end
   end
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def logout(conn) do
@@ -31,15 +45,15 @@ defmodule Rumbl.Auth do
     user = repo.get_by(Rumbl.User, username: username)
 
     cond do
-      user && checkpw(given_pass, user.password_hash) -> {:ok, login(conn, user)}
-      user -> {:error, :unauthorized, conn}
-      true -> dummy_checkpw()
-      {:error, :not_found, conn}
+      user && checkpw(given_pass, user.password_hash) ->
+        {:ok, login(conn, user)}
+      user ->
+        {:error, :unauthorized, conn}
+      true ->
+        dummy_checkpw()
+        {:error, :not_found, conn}
     end
   end
-
-  import Phoenix.Controller
-  alias Rumbl.Router.Helpers
 
   def authenticate_user(conn, _opts) do
     if conn.assigns.current_user do
--- rumbl/web/models/video.ex
+++ rumbl/web/models/video.ex
@@ -7,8 +7,10 @@ defmodule Rumbl.Video do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
+    has_many :annotations, Rumbl.Annotation
 
     timestamps()
   end
@@ -19,7 +21,7 @@ defmodule Rumbl.Video do
   @required_fields ~w(url title description)
   @optional_fields ~w()
 
-  def changeset(model, params \\ :empty) do
+  def changeset(model, params \\ :invalid) do
     model
     |> cast(params, @required_fields, @optional_fields)
     |> slugify_title()
